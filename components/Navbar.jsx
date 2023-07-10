70752bc0bd6072d3b551db7e1fcb3df4001d52c9
sakhyat <sakhyatkabir1@gmail.com>
2023-07-11 02:50:45 +0600

    userbutton added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,26 @@
+import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 
 const Navbar = () => {
+	const { userId } = auth();
+
 	return (
 		<div className="bg-rose-400 text-white">
 			<div className="container mx-aut px-4">
 				<nav className="flex items-center justify-between h-20">
 					<div className="logo text-2xl">Clerk Auth</div>
-					<ul className="links flex gap-5">
-						<li>
-							<Link href="/sign-in">Login</Link>
-						</li>
-						<li>
-							<Link href="/sign-up">Sign Up</Link>
-						</li>
-					</ul>
+					{!userId ? (
+						<ul className="links flex gap-5">
+							<li>
+								<Link href="/sign-in">Login</Link>
+							</li>
+							<li>
+								<Link href="/sign-up">Sign Up</Link>
+							</li>
+						</ul>
+					): (
+						<UserButton afterSignOutUrl="/"/>
+					)}
 				</nav>
 			</div>
 		</div>
